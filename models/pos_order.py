a37275cfa00c9efc080e76ba4eb8b5093238834f
subriaco <116285955+subriaco@users.noreply.github.com>
2022-10-25 11:16:51 -0300

    Agregar Info extra

---
+++ models/pos_order.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+
+from odoo import api, fields, models, tools, _
+
+class PosOrder(models.Model):
+    _inherit = "pos.order"
+    
+    info_extra = fields.Char(string='Info Extra')
