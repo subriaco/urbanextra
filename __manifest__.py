de391ed4a9253ec4b711fe6dd685bf13a720e534
jeffrepo <jeffer.leo93@gmail.com>
2022-10-25 07:43:44 -0600

    update

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Urban extra',
+    'version': '1.0',
+    'category': 'Hidden',
+    'sequence': 6,
+    'summary': 'Módulo reportes Urban',
+    'description': """
+
+""",
+    'depends': ['point_of_sale'],
+    'data': [
+        # 'views/pos_order_report_view.xml',
+    ],
+    'assets':{
+    },
+    'installable': True,
+    'auto_install': False,
+}
